5211c4dea5702b4a3e0cf575707fe8bb2a29b139
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-12-22 19:34:14 -0300

    Apresentando Template literals

--- clientes.js
+++ clientes.js
@@ -4,3 +4,15 @@ const informacoesClientes = [
         nome: "Laura",
     },
 ];
+
+const conteudo = `<tr>
+    <td>${informacoesClientes[0].cpf}</td>
+    <td>${informacoesClientes[0].nome}</td>
+</tr>
+`
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+corpoTabela.innerHTML = conteudo;
+
+console.log(conteudo);
